c8dd58bdcbaff24d859c90cd0273e4f0929bd2f2
Vincent Nguyen <vnguye51@calpoly.edu>
2014-04-03 12:55:00 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Vincent Nguyen"
+git config --global user.email "vnguye51@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
