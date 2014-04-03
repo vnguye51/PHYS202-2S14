8128396dde1b162320b323c20d7b7ecc9a3b99f3
Vincent Nguyen <vnguye51@calpoly.edu>
2014-04-03 13:05:35 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script configures git whenever you start working
+# on multiple machines. Run it any time you decide to work
+# with git on a new computer
+
 git config --global user.name "Vincent Nguyen"
 git config --global user.email "vnguye51@calpoly.edu"
 git config --global color.ui "auto"
